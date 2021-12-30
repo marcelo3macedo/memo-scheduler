8ca4e509b208d68327627ad1ca70c93133026211
Marcelo Macedo <marcelo3macedo@gmail.com>
2021-12-29 23:39:30 -0300

    Ft Postgres

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,5 +1,11 @@
+
+import 'reflect-metadata';
+import 'express-async-errors';
 import express from 'express'
 import { Cron } from './cron'
+
+import '@shared/infra/typeorm';
+import '@shared/container';
 
 const app = express()
 
