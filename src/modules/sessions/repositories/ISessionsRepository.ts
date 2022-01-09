953513d1b1f9e875740a493758b7c129a384d942
Marcelo Macedo <marcelo3macedo@gmail.com>
2022-01-09 14:15:26 -0300

    Add interval logic

--- src/modules/sessions/repositories/ISessionsRepository.ts
+++ src/modules/sessions/repositories/ISessionsRepository.ts
@@ -1,9 +1,11 @@
 import Session from '../entities/Session';
 import ICreateSessionsDTO from "@modules/sessions/dtos/ICreateSessionsDTO";
 import IHasActiveSessionsDTO from '../dtos/IHasActiveSessionsDTO';
+import IFilterSessionsDTO from '../dtos/IFilterSessionsDTO';
 
 export interface ISessionsRepository {
   create(data: ICreateSessionsDTO): Promise<Session>;
   reviewed(): Promise<Session[]>;
   hasActive(data: IHasActiveSessionsDTO): Promise<Session>;
+  filter(data: IFilterSessionsDTO): Promise<Session[]>;
 }
